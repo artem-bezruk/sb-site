1be5eb81bfa505153dc4e1cd839eb26c92691e2b
Artem <artem.web.developer.it@gmail.com>
2021-12-02 07:42:01 +0000

    building products page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -16,6 +16,9 @@ const Navbar = () => (
         <Link className="navbar-item" to="/about">
           About
         </Link>
+        <Link className="navbar-item" to="/products">
+          Products
+        </Link>
       </div>
       <div className="navbar-end">
         <a className="navbar-item" href="https:
