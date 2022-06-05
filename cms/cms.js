c50ac04992258a13b0bd7275044ee1739abb8701
Artem <artem.web.developer.it@gmail.com>
2022-06-05 13:25:55 +0000

    migrate to cms plugin, revert jsx extensions

--- cms/cms.js
+++
@@ -1,8 +0,0 @@
-import CMS from 'netlify-cms';
-import AboutPagePreview from './components/AboutPagePreview';
-import BlogPostPreview from './components/BlogPostPreview';
-import ProductPagePreview from './components/ProductPagePreview';
-CMS.registerPreviewStyle('/styles.css');
-CMS.registerPreviewTemplate('about', AboutPagePreview);
-CMS.registerPreviewTemplate('products', ProductPagePreview);
-CMS.registerPreviewTemplate('blog', BlogPostPreview);
--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BlogPostTemplate } from '../../src/templates/blog-post';
+import { BlogPostTemplate } from '../../templates/blog-post';
 const BlogPostPreview = ({ entry, widgetFor }) => (
   <BlogPostTemplate
     content={widgetFor('body')}
