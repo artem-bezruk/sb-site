e599ca68a6e4c63d13d30ad2005c61c1f5083a36
Artem <artem.web.developer.it@gmail.com>
2023-01-19 05:11:15 +0000

    Navbar mobile view improvements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,37 @@ import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
-const Navbar = () => (
-  <nav className="navbar is-transparent">
+const Navbar = class extends React.Component {
+  componentDidMount() {
+   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+   if ($navbarBurgers.length > 0) {
+     $navbarBurgers.forEach( el => {
+       el.addEventListener('click', () => {
+         const target = el.dataset.target;
+         const $target = document.getElementById(target);
+         el.classList.toggle('is-active');
+         $target.classList.toggle('is-active');
+       });
+     });
+   }
+ }
+ render() {
+   return (
+  <nav className="navbar is-transparent" role="navigation" aria-label="main-navigation">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item" title="Logo">
           <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
         </Link>
+        {}
+        <div class="navbar-burger burger" data-target="navMenu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
-      <div className="navbar-start">
+      <div id="navMenu" className="navbar-menu">
+      <div className="navbar-start has-text-centered">
         <Link className="navbar-item" to="/about">
           About
         </Link>
@@ -24,7 +46,7 @@ const Navbar = () => (
           Form Examples
         </Link>
       </div>
-      <div className="navbar-end">
+      <div className="navbar-end has-text-centered">
         <a
           className="navbar-item"
           href="https:
@@ -36,7 +58,9 @@ const Navbar = () => (
           </span>
         </a>
       </div>
+      </div>
     </div>
   </nav>
-)
+  )}
+}
 export default Navbar
